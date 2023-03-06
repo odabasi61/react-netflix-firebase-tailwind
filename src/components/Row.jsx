0b01d22799fbb01403bde68d4ad1cd7d70464f12
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-03-06 20:04:24 +0300

    row component halfway done

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 
-const Row = () => {
-  return <div></div>;
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((res) => {
+      setMovies(res.data.results);
+    });
+  }, [fetchURL]);
+
+  // console.log(movies);
+
+  return (
+    <div>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex text-center items-center">
+        <div id={"slider"}>
+          {movies.map((item, id) => (
+            <div
+              key={id}
+              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+                alt={item?.title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import Main from "../components/Main";
 import Row from "../components/Row";
+import requests from "../Requests";
+
+// row componentine her bir row için başlık (string şeklinde) ve row türü için url yolladık. reusable row oluşturduk. birden fazla row için kullanıyoruz.
 
 const Home = () => {
   return (
     <div>
-      <Main title="Up Coming" />
-      <Row />
+      <Main />
+      <Row title="Up Coming" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row title="Trending" fetchURL={requests.requestTrending} />
+      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row title="Horror" fetchURL={requests.requestHorror} />
     </div>
   );
 };
