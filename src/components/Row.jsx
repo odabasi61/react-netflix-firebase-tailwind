7a5718f70294f6bc381bededce5d1b51b3350342
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-03-06 22:54:42 +0300

    row completed, firebase next

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,35 +1,54 @@
 import axios from "axios";
-import React from "react";
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
   const [movies, setMovies] = useState([]);
 
+  // fetching the data
   useEffect(() => {
     axios.get(fetchURL).then((res) => {
       setMovies(res.data.results);
     });
   }, [fetchURL]);
-
   // console.log(movies);
+
+  // scrolling the rows in x axis.
+  // home sayfasından rowId adında herbir row için id yolladık. bunu yapmamızın sebebi, sayfadaki hangi tuşa basarsak basalım ilk row hareket ediyor. her birine id aktardık ve her biri bağımsız olarak hareket ediyor.
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <div>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex text-center items-center">
-        <div id={"slider"}>
+      {/* buradaki group, parent dive verildi. yani parent dive hover yapınca çocuk elemanlara etki edilebilmesi için */}
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white rounded-full absolute left-2 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <div
-              key={id}
-              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
-            >
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-            </div>
+            <Movie item={item} key={id} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white rounded-full absolute right-2 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </div>
   );
