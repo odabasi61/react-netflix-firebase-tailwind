b0fcbf7339796efd4ac6f61753bdbe3c64cf8bec
Ali ODABAŞI <aliodabas_a@hotmail.com>
2023-03-07 16:57:01 +0300

    login logout and account completed

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,34 @@
+import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Movie = ({ item }) => {
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = UserAuth();
+
+  const movieID = doc(db, "users", `${user?.email}`);
+
+  const saveShow = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
 
   return (
-    <div className="w-[200px] sm:w-[220px] md:w-[260px] lg:w-[300px] inline-block cursor-pointer relative p-2">
+    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
         className="w-full h-auto block"
         src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
@@ -15,11 +38,11 @@ const Movie = ({ item }) => {
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {item?.title}
         </p>
-        <p>
+        <p onClick={saveShow}>
           {like ? (
-            <FaHeart className="absolute top-4 left-4" />
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
-            <FaRegHeart className="absolute top-4 left-4" />
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
           )}
         </p>
       </div>
